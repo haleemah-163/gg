7811d738b88da6306877ad77fda22f423e6e2268
haleemah-163 <abolaimon777@gmail.com>
2021-01-04 21:17:17 +0200

    hhh

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 
-var phott = alert('حط بعقلك رقم');
-var ptake = alert('خذ من حسن قده'); 
+ alert('حط بعقلك رقم');
+alert('خذ من حسن قده'); 
 var hot = prompt('الرقم اللي بدك اياه اخذه مني واكتبه هون بالانجليزي');
 var dal = hot/2;
-var nos =  (phott + ptake + hot )/2;
-var raj = ( nos - ptake );
+alert('كب نصهن بالبحر');
 alert('رجع لحسن رقمه');
 alert('احزر قديش ضل معك؟');
 alert(dal);
