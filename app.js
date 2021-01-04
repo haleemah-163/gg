dab215ee86e2788687afbd9be71ffaf236391647
haleemah-163 <abolaimon777@gmail.com>
2021-01-04 21:03:29 +0200

    tryingpuzzle

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var phott = alert('حط بعقلك رقم');
+var ptake = alert('خذ من حسن قده'); 
+var hot = prompt('الرقم اللي بدك اياه اخذه مني واكتبه هون بالانجليزي');
+var dal = hot/2;
+var nos =  (phott + ptake + hot )/2;
+var raj = ( nos - ptake );
+alert('رجع لحسن رقمه');
+alert('احزر قديش ضل معك؟');
+alert(dal);
+
